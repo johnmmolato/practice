b1f34650a61b5e9f67b1a7befadab1462ad0eea8
johnmmolato <93026051+johnmmolato@users.noreply.github.com>
2022-09-13 06:29:35 -0700

    chaged

--- script.js
+++ script.js
@@ -1,102 +1,18 @@
-const APP = {
-  TOKEN: "pk.2f18ebbf211350dd5e3f51dfe848b5d0",
-  SEARCHURL: `https://us1.locationiq.com/v1/search?format=json&`,
-  REVERSEURL: `https://us1.locationiq.com/v1/reverse?format=json&`,
-  MAPURL: `https://maps.locationiq.com/v3/staticmap?`,
-  data: null,
-  init: () => {
-    document
-      .getElementById("btnSearch")
-      .addEventListener("click", APP.doSearch);
-    document
-      .getElementById("btnReverse")
-      .addEventListener("click", APP.doReverse);
-    document.getElementById("btnMap").addEventListener("click", APP.getMap);
-  },
-  doSearch: (ev) => {
-    ev.preventDefault();
-    //use forward geocoding
-    let q = document.getElementById("keyword").value.trim();
-    if (!q) return false;
-    let url = `${APP.SEARCHURL}key=${APP.TOKEN}&q=${q}`;
-    //call the API and do a forward geocoding search
-    //save the results in a global location
-    fetch(url)
-      .then((resp) => {
-        if (!resp.ok) throw new Error(resp.statusText);
-        return resp.json();
-      })
-      .then((data) => {
-        APP.data = data[0];
-        APP.showSearchResults();
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  },
-  showSearchResults: () => {
-    //display the results of the search
-    console.log(APP.data);
-    let section = document.querySelector(".results");
-    let pre = section.querySelector("pre");
-    if (!pre) {
-      pre = document.createElement("pre");
-      section.append(pre);
-    }
-    //just dump the data response into the <pre> element
-    //just the first result from the array
-    pre.textContent = JSON.stringify(APP.data, null, 2);
-  },
-  doReverse: (ev) => {
-    ev.preventDefault();
-    let q = document.getElementById("keyword").value.trim();
-    //validation
-    if (!q) return false; //exit if empty
-    if (q.indexOf(",") < 0) return false; //exit if no comma
-    let parts = q.split(","); //make array with 2 parts
-    if (parts.length !== 2 || isNaN(parts[0]) || isNaN(parts[1])) return false;
-    //exit if not number lat or lon, not two values
-    //build url
-    let url = `${APP.REVERSEURL}key=${APP.TOKEN}&lat=${parts[0]}&lon=${parts[1]}`;
-    //do a reverse geocoding call
-    //save the results in a global location
-    fetch(url)
-      .then((resp) => {
-        if (!resp.ok) throw new Error(resp.statusText);
-        return resp.json();
-      })
-      .then((data) => {
-        console.log(data);
-        APP.data = data; //no [0]
-        APP.showSearchResults();
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  },
-  getMap: (ev) => {
-    ev.preventDefault();
-    if (!APP.data) return false; //make sure there is data
-    let lon = APP.data.lon; //get the longitude from the last retrieved
-    let lat = APP.data.lat; //get the longitude from the last retrieved
-    //build the URL with center, zoom, size, format
-    let url = `${APP.MAPURL}key=${APP.TOKEN}&center=${lat},${lon}&zoom=9&size=400x600&format=png`;
-    //display the static map
-    APP.showMap(url);
-  },
-  showMap: (url) => {
-    let section = document.querySelector(".map");
-    let img = section.querySelector("img");
-    if (!img) {
-      img = document.createElement("img");
-      section.append(img);
-    }
-    img.alt = APP.data.display_name; // get the display_name from the data
-    // put some ref into an attribute
-    img.setAttribute("data-place-id", APP.data.place_id);
-    // set the src with the url param
-    img.src = url;
-  },
-};
+document.getElementById("search-btn").addEventListener("click", (evt) => {
+  evt.preventDefault();
+  L.mapquest.key = "DOEEESf7yldIWHelbTOJkQ3KzaxXWrA7";
 
-document.addEventListener("DOMContentLoaded", APP.init);
+  L.mapquest.geocoding().geocode("Boston, MA", createMap);
+
+  function createMap(error, response) {
+    var location = response.results[0].locations[0];
+    var latLng = location.displayLatLng;
+    // 'map' refers to a <div> element with the ID map
+    L.mapquest.map("map", {
+      center: latLng,
+      // L.mapquest.tileLayer('map') is the MapQuest map tile layer
+      layers: L.mapquest.tileLayer("map"),
+      zoom: 12,
+    });
+  }
+});

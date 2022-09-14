58df5d9a93e25884be66928a269edc5ffc928146
johnmmolato <93026051+johnmmolato@users.noreply.github.com>
2022-09-13 22:28:27 -0700

    improve

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const locationMap = function () {
-  L.mapquest.key = "DOEEESf7yldIWHelbTOJkQ3KzaxXWrA7";
+const key = "DOEEESf7yldIWHelbTOJkQ3KzaxXWrA7";
 
+const locationMap = function () {
+  L.mapquest.key = key;
   const locationValue = document.getElementById("inputLocation").value;
   document.getElementById("user_input").reset();
 
   L.mapquest.geocoding().geocode(locationValue, createMap);
+
+  const urlMap = `https://www.mapquestapi.com/geocoding/v1/address?key=${key}&location=${locationValue}`;
+
+  fetch(urlMap)
+    .then((resp) => resp.json())
+    .then((data) => console.log(data));
 
   function createMap(error, response) {
     const location = response.results[0].locations[0];
@@ -16,36 +23,4 @@ const locationMap = function () {
     });
   }
 };
-document.getElementById("btn_seach").addEventListener(
-  "click",
-  locationMap
-
-  //document.getElementById("user_input").reset();
-  // const key = "DOEEESf7yldIWHelbTOJkQ3KzaxXWrA7";
-  // const locationValue = document.getElementById("inputLocation").value;
-
-  // //mapquest object to use for longtitude and latitude
-  // const urlMap = `https://www.mapquestapi.com/geocoding/v1/address?key=${key}&location=${locationValue}`;
-
-  // fetch(urlMap)
-  //   .then((resp) => resp.json())
-  //   .then((data) => console.log(data));
-
-  // document
-  //   .getElementById("btn_seach")
-  //   .addEventListener("click", searchLocation);
-  // const searchLocation = L.mapquest
-  //   .geocoding()
-  //   .geocode("Boston, MA", createMap);
-);
-
-// const mapBoxKey = pk.eyJ1IjoibWFyaW5lczgzMDIiLCJhIjoiY2w4MGRyZzF0MDAwazN3czVzOHZtNG92bSJ9.wp0JFr7-mf9vQE6sHkntFQ
-
-// const mapLocation = L.map('map').setView([51.505, -0.09], 13);
-// https://api.mapbox.com/{endpoint}?access_token={your_access_token}
-
-// mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
-// const map = new mapboxgl.Map({
-//   container: 'map',
-//   style: 'mapbox://styles/mapbox/streets-v11'
-// });
+document.getElementById("btn_seach").addEventListener("click", locationMap);
